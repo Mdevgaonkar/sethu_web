64729de8ce4660167f99605ccf0c21a6b2da5d66
mayur_d_developer <dmayur57@gmail.com>
2018-04-26 08:30:00 +0530

    adding data from index.json

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const data = require('../data/index.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // console.log(data);
+  res.locals.title=data.title;
+  res.locals.data=data;
+  res.render('index');
 });
 
 module.exports = router;
