8409a9049214eba4e1e4915384b2b10ea4f3a478
mayur_d_developer <dmayur57@gmail.com>
2018-05-02 11:32:59 +0530

    added reloadify and changed nodemon cmd

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var users = require('./routes/users');
 
 var app = express();
 
+
 // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
 nunjucks.configure('views', {
@@ -18,7 +19,8 @@ nunjucks.configure('views', {
   express: app
 });
 app.set('view engine', 'html');
-
+var reloadify = require('reloadify')(__dirname + '/views');
+app.use(reloadify);
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
